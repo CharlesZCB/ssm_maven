3b2d9fbd7d539ddb9d64b76b78b4117ea32db99e
delta.zhang <2365956483@qq.com>
2018-12-08 20:12:21 +0800

    first commit

---
+++ src/main/java/com/cn/serviceImp/UserServiceImp.java
@@ -0,0 +1,6 @@
+package com.cn.serviceImp;
+
+import com.cn.service.UserService;
+
+public class UserServiceImp implements UserService {
+}
---
+++ target/maven01/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Hello World! Mr Maven</h2>
+</body>
+</html>
